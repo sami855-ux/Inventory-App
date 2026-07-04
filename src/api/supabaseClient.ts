3c6f82cabc7cae94cfd7bf332887e2579dfd43a0
sami855-ux <samitale56@outlook.com>
2026-07-04 16:40:02 +0300

    some updates

--- src/api/supabaseClient.ts
+++ src/api/supabaseClient.ts
@@ -1,6 +1,4 @@
+import { env } from "@/src/config/env"
 import { createClient } from "@supabase/supabase-js"
 
-export const supabase = createClient(
-  "https://urvjuijmbykiwxphqjnk.supabase.co",
-  "sb_publishable_chT5g-27t8FdB4E4MnijpQ_z_O5agJ6",
-)
+export const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_KEY)
